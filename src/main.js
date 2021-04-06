93871ccfa2f3a972f702bd689491a2814b21700a
KristopherYu <74690002+KristopherYu@users.noreply.github.com>
2021-04-05 17:08:40 -0700

    Add coe

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ console.log("This is da patrol, rocket patrol")
 let config = {
     type: Phaser.CANVAS,
     width: 640,
-    height: 480
+    height: 480,
+    scene: [ Menu, Play ]
 }
 let game = new Phaser.Game(config);
